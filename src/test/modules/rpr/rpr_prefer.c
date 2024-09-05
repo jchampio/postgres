7ea2498b3f6ebfd881437a6b5d673dae827021eb
Jacob Champion <jacob.champion@enterprisedb.com>
2024-09-05 10:46:43 -0700

    WIP: add skeleton for RPR preferment

---
+++ src/test/modules/rpr/rpr_prefer.c
@@ -0,0 +1,19 @@
+/*-------------------------------------------------------------------------
+ *
+ * rpr_prefer.c
+ *    TODO
+ *
+ * Copyright (c) 2024, PostgreSQL Global Development Group
+ *
+ * IDENTIFICATION
+ *    src/test/modules/rpr/rpr_prefer.c
+ *
+ * TODO
+ *
+ *-------------------------------------------------------------------------
+ */
+
+int main()
+{
+	return 0;
+}
